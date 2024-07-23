aeb1f02c45737ffeed6773b9018c00c2efd716f5
tomasjrl <tstabilini@gmail.com>
2024-07-23 19:58:59 -0300

    agregado productos.js como 2do script al HTML

--- js/index.js
+++ js/index.js
@@ -1,79 +1,9 @@
-
-// guardada la estructura de datos de la lista de productos
-// se implementara en index.html generando codigo html desde javascript
-// precio en centavos para evitar conflictos con redondeos posteriores
-
-const productos = [
-  {
-    imagen: "assets/images/1.webp",
-    marca: "TRES TORRES",
-    contenido: "Amargo",
-    medida: "x2lt",
-    precioCentavos: 99900,
-  },
-  {
-    imagen: "assets/images/2.webp",
-    marca: "LA QUESERA",
-    contenido: "Queso azul",
-    medida: "x100gr",
-    precioCentavos: 119900,
-  },
-  {
-    imagen: "assets/images/3.webp",
-    marca: "MATARAZZO",
-    contenido: "Fideos varios",
-    medida: "x500gr",
-    precioCentavos: 94900,
-  },
-  {
-    imagen: "assets/images/4.webp",
-    marca: "SKYY",
-    contenido: "Vodka sabores",
-    medida: "x700cc",
-    precioCentavos: 99900,
-  },
-  {
-    imagen: "assets/images/5.webp",
-    marca: "NIVEA MEN",
-    contenido: "Antitranspirante aerosol",
-    medida: "x150ml",
-    precioCentavos: 168900,
-  },
-  {
-    imagen: "assets/images/6.webp",
-    marca: "POETT",
-    contenido: "Desodorante de ambiente",
-    medida: "x360ml",
-    precioCentavos: 143500,
-  },
-  {
-    imagen: "assets/images/7.webp",
-    marca: "LUCCHETTI",
-    contenido: "Arroz largo fino",
-    medida: "Bolsa x1kg",
-    precioCentavos: 158900,
-  },
-  {
-    imagen: "assets/images/8.webp",
-    marca: "DOLCA",
-    contenido: "Café instantáneo frasco",
-    medida: "x100gr",
-    precioCentavos: 258900,
-  },
-  {
-    imagen: "assets/images/9.webp",
-    marca: "CHAMIGO",
-    contenido: "Yerba",
-    medida: "x500gr",
-    precioCentavos: 99900,
-  },
-];
-
 // creo variable productosHTML para insertar el html generado con todos los productos agregados
 // esta variable debe estar antes de la funcion productos.forEach((producto) => {}
 let productosHTML = '';
 
-// funcion para recorrer de "productos" cada objeto
+// funcion forEach para recorrer cada objeto de productos.js
+// productos.js esta cargado ANTES que index.js en index.html
 // se le da como parametro el nombre = "producto"
 // divido a precioCentavos por 100 para que el resultado sea en pesos y utilizo la funcion .toFixed(2) para que muestre los decimales
 // ejecuto un patron de acumulacion += para que sume el contenido obtenido de cada producto a la variable productosHTML
---
+++ js/productos.js
@@ -0,0 +1,78 @@
+// guardada la estructura de datos de la lista de productos
+// se implementara en index.html generando codigo html desde javascript
+// precio en centavos para evitar conflictos con redondeos posteriores
+
+const productos = [
+  {
+    identificador: 'qazqazqaz',
+    imagen: "assets/images/1.webp",
+    marca: "TRES TORRES",
+    contenido: "Amargo",
+    medida: "x2lt",
+    precioCentavos: 99900,
+  },
+  {
+    identificador: 'wsxwsxwsx',
+    imagen: "assets/images/2.webp",
+    marca: "LA QUESERA",
+    contenido: "Queso azul",
+    medida: "x100gr",
+    precioCentavos: 119900,
+  },
+  {
+    identificador: 'edcedcedc',
+    imagen: "assets/images/3.webp",
+    marca: "MATARAZZO",
+    contenido: "Fideos varios",
+    medida: "x500gr",
+    precioCentavos: 94900,
+  },
+  {
+    identificador: 'tgbtgbtgb',
+    imagen: "assets/images/4.webp",
+    marca: "SKYY",
+    contenido: "Vodka sabores",
+    medida: "x700cc",
+    precioCentavos: 99900,
+  },
+  {
+    identificador: 'yhnyhnyhn',
+    imagen: "assets/images/5.webp",
+    marca: "NIVEA MEN",
+    contenido: "Antitranspirante aerosol",
+    medida: "x150ml",
+    precioCentavos: 168900,
+  },
+  {
+    identificador: 'ujmujmujm',
+    imagen: "assets/images/6.webp",
+    marca: "POETT",
+    contenido: "Desodorante de ambiente",
+    medida: "x360ml",
+    precioCentavos: 143500,
+  },
+  {
+    identificador: 'ewqewqewq',
+    imagen: "assets/images/7.webp",
+    marca: "LUCCHETTI",
+    contenido: "Arroz largo fino",
+    medida: "Bolsa x1kg",
+    precioCentavos: 158900,
+  },
+  {
+    identificador: 'ytrytrytr',
+    imagen: "assets/images/8.webp",
+    marca: "DOLCA",
+    contenido: "Café instantáneo frasco",
+    medida: "x100gr",
+    precioCentavos: 258900,
+  },
+  {
+    identificador: 'oiuoiuoiu',
+    imagen: "assets/images/9.webp",
+    marca: "CHAMIGO",
+    contenido: "Yerba",
+    medida: "x500gr",
+    precioCentavos: 99900,
+  },
+];
