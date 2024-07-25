7d1eeb84d3137b6b2376cc25b99541701453b0a3
tomasjrl <tstabilini@gmail.com>
2024-07-25 04:16:13 -0300

    agrego cuentas interactivas en console.log + en html + nuevos productos + nuevas imagenes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-// creo variable productosHTML para insertar el html generado con todos los productos agregados
-// esta variable debe estar antes de la funcion productos.forEach((producto) => {}
+/*--------------------------------------------------------------//
+
+      FUNCION PARA AGREGAR AL HTML TODOS LOS PRODUCTOS DESDE JAVASCRIPT
+      
+//--------------------------------------------------------------*/
 
 let productosHTML = "";
 
-// funcion forEach para recorrer cada objeto de productos.js
-// productos.js esta cargado ANTES que index.js en index.html
-// se le da como parametro el nombre = "producto"
+// creo variable productosHTML para insertar el html generado con todos los productos
+// La funcion forEach recorre cada objeto de productos.js
+// para que sume el contenido obtenido de cada producto a productosHTML
 
 productos.forEach((producto) => {
-  // ejecuto un patron de acumulacion += para que sume el contenido obtenido de cada producto a la variable productosHTML
+
 
   productosHTML += `
-  <div class="producto-contenedor">
+  <div class="producto-contenedor js-producto-contenedor" 
+     data-producto-id="${producto.identificador}"
+     data-producto-marca="${producto.marca}"
+     data-producto-contenido="${producto.contenido}"
+     data-producto-medida="${producto.medida}">
         <div>
           <img src="${producto.imagen}">
         </div>
@@ -19,67 +26,128 @@ productos.forEach((producto) => {
           <div class="marca-producto">${producto.marca}</div>
           <div class="contenido-producto">${producto.contenido}</div>
           <div class="medida-producto">${producto.medida}</div>
-          <div class="precio-producto">$${
-            // divido a precioCentavos por 100 para que el resultado sea en pesos y utilizo la funcion .toFixed(2) para que muestre los decimales
-
-            (producto.precioCentavos / 100).toFixed(2)
-          }</div>
+          <div class="precio-producto">$${(producto.precio)}</div>
         </section>
-        <div>
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
         <div class="botones-agregar-cancelar">
           <button class="boton-agregar js-boton-agregar" 
           data-producto-id="${producto.identificador}" 
           data-producto-marca="${producto.marca}"
           data-producto-contenido="${producto.contenido}" 
           data-producto-medida="${producto.medida}"
+          data-producto-precio="${(producto.precio)
+              }"
           >AGREGAR</button>
-          <button class="boton-cancelar">Cancelar</button>
         </div>
       </div>
  `;
 });
 
-// traigo a javascript la etiqueta con clase "js-productos-grid"
+// traigo a javascript la etiqueta con clase "js-productos-grid" 
 // y agrego al html el codigo obtenido en la variable productosHTML
 
 document.querySelector(".js-productos-grid").innerHTML = productosHTML;
 
-// traigo a javascript la etiqueta con clase "js-boton-agregar" para modificar su contenido
-// aplico forEach porque debe fijarse segun cada boton de cada producto
+/*--------------------------------------------------------------//
+
+                FUNCION PARA BUSCAR PRODUCTOS
+
+//--------------------------------------------------------------*/
+
+function buscarProductos() {
+
+  const terminoBusqueda = document
+    .querySelector("#buscador")
+    .value.toLowerCase();
+
+  const productosElementos = document.querySelectorAll(".js-producto-contenedor");
+
+  // Obtengo todos los elementos de la clase "js-producto-contenedor"
+  // Recorro todos los elementos de productos y sus atributos data
+
+  productosElementos.forEach((productoElemento) => {
+
+    const id = productoElemento.getAttribute("data-producto-id") || "";
+    const marca = productoElemento.getAttribute("data-producto-marca") || "";
+    const contenido =
+      productoElemento.getAttribute("data-producto-contenido") || "";
+    const medida = productoElemento.getAttribute("data-producto-medida") || "";
+
+    // Verifico si alguno de los atributos contiene el término de búsqueda
+
+    if (
+      marca.toLowerCase().includes(terminoBusqueda) ||
+      contenido.toLowerCase().includes(terminoBusqueda) ||
+      medida.toLowerCase().includes(terminoBusqueda)
+    ) {
+      productoElemento.style.display = "";
+    } else {
+      productoElemento.style.display = "none";
+    }
+  });
+}
+
+// event listener al campo de búsqueda para que se ejecute la función al escribir
+
+document.querySelector("#buscador").addEventListener("input", buscarProductos);
+
+
+/*--------------------------------------------------------------//
+
+      FUNCION AL APRETAR EL BOTON "AGREGAR" PRODUCTO AL CARRITO DE COMPRAS
+
+//--------------------------------------------------------------*/
+
 
 document.querySelectorAll(".js-boton-agregar").forEach((boton) => {
-  // utilizo 'addEventListener' para escuchar el click del usuario sobre el boton
-
   boton.addEventListener("click", () => {
-    // agrego un alert para indicar si desea confirmar el producto
-    // agrego un 2do alert si es confirmado por el usuario
+    let cantidad;
 
-    if (confirm("¿Desea agregar este producto al carrito de compras?")) {
-      alert("Producto agregado al carrito de compras");
+    // agrego un alert para indicar si desea confirmar el producto
+    // agrego un confirm para que indique la cantidad de productos
+    // agrego un 2do alert confirmando la cantidad agregada
+
+    if (
+      confirm(
+        "¿Confirma este producto al carrito de compras?\n\n(esta información será agregada al console.log)"
+      )
+    ) {
+      while (true) {
+        let input = prompt(
+          "Ingrese la cantidad de este producto que desea agregar:"
+        );
+
+        if (input === null) {
+          alert("Proceso cancelado.");
+          return;
+        }
+
+        cantidad = parseInt(input);
+
+        if (!isNaN(cantidad) && cantidad > 0) {
+          break;
+        } else {
+          alert(
+            "Cantidad no válida. Pruebe nuevamente ingresando un número mayor a 0."
+          );
+        }
+      }
+
+      alert(
+        "Agregado al carrito de compras\n\n(información agregada al console.log)"
+      );
       const productoId = boton.dataset.productoId;
       const productoMarca = boton.dataset.productoMarca;
       const productoContenido = boton.dataset.productoContenido;
       const productoMedida = boton.dataset.productoMedida;
+      const productoPrecio = boton.dataset.productoPrecio;
 
-      // funcion para agregar productos a la compra
-      // si ya existe agrega solo incrementa +1 a la cantidad (ya que es el mismo producto)
-      // si no existe agrega los valores del producto a la compra
+/*--------------------------------------------------------------//
 
+      FUNCION PARA ACTUALIZAR CANTIDAD EN EL CARRITO (y en el console.log)
+
+//--------------------------------------------------------------*/
       let matchingItem;
+      let subtotal = parseFloat(productoPrecio) * cantidad;
 
       compra.forEach((item) => {
         if (productoId === item.productoId) {
@@ -88,31 +156,57 @@ document.querySelectorAll(".js-boton-agregar").forEach((boton) => {
       });
 
       if (matchingItem) {
-        // si existe sumo +1 la "cantidad"
-        matchingItem.cantidad += 1;
-      } else {
-        // envio con push a compras.js los nuevos valores del producto agregado confirmado por el agregado
-
+        matchingItem.cantidad += cantidad;
+        matchingItem.subtotal += subtotal;
+      } 
+      else {
         compra.push({
           productoId: productoId,
           productoMarca: productoMarca,
           productoContenido: productoContenido,
           productoMedida: productoMedida,
-          cantidad: 1,
+          productoPrecio: productoPrecio,
+          cantidad: cantidad,
+          subtotal: subtotal,
         });
       }
+
+      let cantidadDeCompras = 0;
+      let sumaDeCompras = 0;
+
+    // si ya existe agrega solo incrementa la cantidad especificada
+    // si no existe agrega los valores del producto a la compra
+
+      compra.forEach((item) => {
+        cantidadDeCompras += item.cantidad;
+        sumaDeCompras += item.subtotal;
+      });
+
+      document.querySelector(".js-cantidad-compras").innerHTML =
+      `${cantidadDeCompras}`;
+      document.querySelector(".js-suma-compras").innerHTML =
+        `$${sumaDeCompras}`;
+        document.querySelector(".js-pago-total").innerHTML =
+        `$${(sumaDeCompras * 1.21).toFixed(2)}`;
+
+      console.log(
+        "%cNUEVO PRODUCTO AGREGADO",
+        "text-decoration: underline; color: red; font-weight: bold;"
+      );
+      console.log(`${productoMarca} ${productoContenido} ${productoMedida}`);
+      console.log(`Precio $${productoPrecio} * Unidades ${cantidad} = $${(parseFloat(productoPrecio) * parseInt(cantidad))}`);
+      console.log(
+        `%cSUB-TOTAL = $${sumaDeCompras} // TOTAL UNIDADES = ${cantidadDeCompras}`,
+        "color: lightblue; font-weight: bold;"
+      );
+      console.log(
+        "IVA: * 1.21"
+      );
+      console.log(
+        `%cTOTAL = $${(sumaDeCompras * 1.21).toFixed(2)}`,
+        "color: lightgreen; font-weight: bold;"
+      );
+      
     }
-
-    // actualiza numero de "cantidad" de compras para seccion "hero" del html
-
-    let cantidadDeCompras = 0;
-
-    compra.forEach((item) => {
-      cantidadDeCompras += item.cantidad;
-    });
-
-    // traigo a javascript la etiqueta 'js-cantidad-compras'
-    // reescribo el html con la cantidad actualizada
-    document.querySelector('.js-cantidad-compras').innerHTML = cantidadDeCompras;
   });
 });
--- js/productos.js
+++ js/productos.js
@@ -1,78 +1,137 @@
-// guardada la estructura de datos de la lista de productos
-// se implementara en index.html generando codigo html desde javascript
+/*--------------------------------------------------------------//
+
+      ESTRUCTURA DE LISTAS DE PRODUCTOS
+
+//--------------------------------------------------------------*/
+
+
 // precio en centavos para evitar conflictos con redondeos posteriores
 
 const productos = [
   {
-    identificador: 'qazqazqaz',
+    identificador: '00000001',
     imagen: "assets/images/1.webp",
     marca: "TRES TORRES",
     contenido: "Amargo",
     medida: "x2lt",
-    precioCentavos: 99900,
+    precio: 999,
+    
   },
   {
-    identificador: 'wsxwsxwsx',
+    identificador: '00000002',
     imagen: "assets/images/2.webp",
     marca: "LA QUESERA",
     contenido: "Queso azul",
     medida: "x100gr",
-    precioCentavos: 119900,
+    precio: 1199,
+    
   },
   {
-    identificador: 'edcedcedc',
+    identificador: '00000003',
     imagen: "assets/images/3.webp",
     marca: "MATARAZZO",
     contenido: "Fideos varios",
     medida: "x500gr",
-    precioCentavos: 94900,
+    precio: 949,
+    
   },
   {
-    identificador: 'tgbtgbtgb',
+    identificador: '00000004',
     imagen: "assets/images/4.webp",
     marca: "SKYY",
     contenido: "Vodka sabores",
     medida: "x700cc",
-    precioCentavos: 99900,
+    precio: 999,
+    
   },
   {
-    identificador: 'yhnyhnyhn',
+    identificador: '00000005',
     imagen: "assets/images/5.webp",
     marca: "NIVEA MEN",
     contenido: "Antitranspirante aerosol",
     medida: "x150ml",
-    precioCentavos: 168900,
+    precio: 1689,
+    
   },
   {
-    identificador: 'ujmujmujm',
+    identificador: '00000006',
     imagen: "assets/images/6.webp",
     marca: "POETT",
     contenido: "Desodorante de ambiente",
     medida: "x360ml",
-    precioCentavos: 143500,
+    precio: 1435,
+    
   },
   {
-    identificador: 'ewqewqewq',
+    identificador: '00000007',
     imagen: "assets/images/7.webp",
     marca: "LUCCHETTI",
     contenido: "Arroz largo fino",
     medida: "Bolsa x1kg",
-    precioCentavos: 158900,
+    precio: 1589,
+    
   },
   {
-    identificador: 'ytrytrytr',
+    identificador: '00000008',
     imagen: "assets/images/8.webp",
     marca: "DOLCA",
     contenido: "Café instantáneo frasco",
     medida: "x100gr",
-    precioCentavos: 258900,
+    precio: 2589,
+    
   },
   {
-    identificador: 'oiuoiuoiu',
+    identificador: '00000009',
     imagen: "assets/images/9.webp",
     marca: "CHAMIGO",
     contenido: "Yerba",
     medida: "x500gr",
-    precioCentavos: 99900,
+    precio: 999,
+    
+  },
+  {
+    identificador: '00000010',
+    imagen: "assets/images/10.webp",
+    marca: "COCA-COLA",
+    contenido: "Sabor Original",
+    medida: "x2.25L",
+    precio: 3100,
+    
+  },
+  {
+    identificador: '00000011',
+    imagen: "assets/images/11.webp",
+    marca: "COCA-COLA",
+    contenido: "Zero",
+    medida: "x2.25L",
+    precio: 3100,
+    
+  },
+  {
+    identificador: '00000012',
+    imagen: "assets/images/12.webp",
+    marca: "CORONA",
+    contenido: "Cerveza Corona Rubia",
+    medida: "x710cc",
+    precio: 2534,
+    
+  },
+  {
+    identificador: '00000013',
+    imagen: "assets/images/13.webp",
+    marca: "LA SERENISIMA",
+    contenido: "Liviana",
+    medida: "Bote x1L",
+    precio: 1929,
+    
+  },
+  {
+    identificador: '00000014',
+    imagen: "assets/images/14.webp",
+    marca: "MANI KING",
+    contenido: "Salado Sin Piel",
+    medida: "x100gr",
+    precio: 111,
+    
   },
 ];
