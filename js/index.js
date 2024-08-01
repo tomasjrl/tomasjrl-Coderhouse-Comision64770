5b3dc9e922316c50984837da81467e667610a650
tomasjrl <tstabilini@gmail.com>
2024-08-01 02:38:50 -0300

    genero section-hero desde javascript

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-let listadoDeCompra = [];
-
+import { crearHeroSection } from "./html/hero-html.js";
 import { ordenarProductos } from "./html/ordenarProductos.js";
 import { buscarProductos } from "./html/buscador.js";
 import { actualizarTotales } from "./utils/cuenta-compras.js";
 import { procesoCompra } from "./utils/proceso-compras.js";
 export { listadoDeCompra };
 
+let listadoDeCompra = [];
+
+crearHeroSection();
 
 ordenarProductos();
 
