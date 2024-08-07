3c393a8d98d6a924b9e29d3d80b471ab5a951ace
tomasjrl <tstabilini@gmail.com>
2024-08-07 03:20:42 -0300

    agrego orden alfabetico de productos (svg)

--- js/html/ordenarProductos.js
+++ js/html/ordenarProductos.js
@@ -1,8 +1,26 @@
 import { productos } from "../data/productos.js";
 import { generarHTMLProductos } from "./contenedor-html.js";
 
+document.addEventListener("DOMContentLoaded", function () {
+  const botonOrdenar = document.querySelector(".orden-productos-svg");
+  botonOrdenar.addEventListener("click", ordenarProductos);
+});
+
+let ordenAlfabetico = true;
+
 export function ordenarProductos() {
-  productos.sort((a, b) => a.marca.localeCompare(b.marca));
+  const botonOrdenar = document.querySelector(".orden-productos-svg");
+  const invertido = !ordenAlfabetico;
+
+  productos.sort((a, b) => {
+    const comparacion = a.marca.localeCompare(b.marca);
+    return invertido ? -comparacion : comparacion;
+  });
+
+  ordenAlfabetico = invertido;
+  botonOrdenar.classList.toggle("invertido", invertido);
+  localStorage.setItem("ordenInvertido", invertido);
+
   let htmlProductos = generarHTMLProductos(productos);
   document.querySelector(".js-productos-grid").innerHTML = htmlProductos;
 }
