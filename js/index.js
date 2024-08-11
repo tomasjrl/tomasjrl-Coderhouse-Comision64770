1000eddf12f9539cb12840c4c496873a15f95a82
tomasjrl <tstabilini@gmail.com>
2024-08-11 03:56:34 -0300

    asigno timeout al inicio de index

--- js/index.js
+++ js/index.js
@@ -5,23 +5,26 @@ import { actualizarTotales , listadoDeCompra } from "./utils/cuenta-compras.js";
 import { procesoCompra } from "./utils/proceso-compras.js";
 import { toggleMode } from "./html/dark-mode.js";
 
-/* llamo a la funcion que permite cambiar modo claro-oscuro */
-toggleMode();
 
-/* llamo a la funcion para generar el contenido HTML que estará dentro de <section class="hero-section"> 
-Aquí esta ubicado el listado de compras, las cuentas y la opción de pagar/cancelar la operación*/
-crearHeroSection();
+setTimeout(() => {
+  /* llamo a la funcion que permite cambiar modo claro-oscuro */
+  toggleMode();
 
-/* llamo a la funcion que permite buscar/encontrar productos según parámetros de búsqueda*/
-buscarProductos();
+  /* llamo a la funcion para generar el contenido HTML que estará dentro de <section class="hero-section"> 
+  Aquí esta ubicado el listado de compras, las cuentas y la opción de pagar/cancelar la operación*/
+  crearHeroSection();
 
-/* llamo a la funcion que permite generar el contenido HTML que comprende a las cartas de productos
-que estarán dentro de <section class="productos-grid js-productos-grid"> */
-ordenarProductos();
+  /* llamo a la funcion que permite buscar/encontrar productos según parámetros de búsqueda*/
+  buscarProductos();
 
-/* llamo a la funcion que permite actualizar las cuentas luego de agregar/quitar productos al listado de compras*/
-actualizarTotales();
+  /* llamo a la funcion que permite generar el contenido HTML que comprende a las cartas de productos
+  que estarán dentro de <section class="productos-grid js-productos-grid"> */
+  ordenarProductos();
 
-/* llamo a la funcion que ejecuta la logica del procesamiento de datos entre el click del usuario en los botones 
-(agregar / cancelar / pagar total / cancelar total) en relación a los valores de los productos para agregarlo al listado de compras  */
-procesoCompra(listadoDeCompra);
+  /* llamo a la funcion que permite actualizar las cuentas luego de agregar/quitar productos al listado de compras*/
+  actualizarTotales();
+
+  /* llamo a la funcion que ejecuta la logica del procesamiento de datos entre el click del usuario en los botones 
+  (agregar / cancelar / pagar total / cancelar total) en relación a los valores de los productos para agregarlo al listado de compras  */
+  procesoCompra(listadoDeCompra);
+}, 100); // retraso de 100 milisegundos
--- js/utils/proceso-compras.js
+++ js/utils/proceso-compras.js
@@ -1,5 +1,5 @@
 import { botonesProductos } from "./boton-agregar.js";
-import { actualizarTotales } from "./cuenta-compras.js";
+import { restablecerCompra, agregarEventoBoton } from "./boton-pagar.js";
 
 let tipo = null;
 
@@ -14,65 +14,8 @@ export function procesoCompra(listadoDeCompra) {
      BOTON PARA PAGAR / CANCELAR LA COMPRA TOTAL
   //--------------------------------------------------------------*/
 
-  function restablecerCompra(tipo) {
-    if (tipo === null || tipo === undefined) return;
-  
-    const botones = document.querySelectorAll(".js-boton-hero");
-  
-    botones.forEach((boton) => {
-      boton.disabled = true;
-    });
-  
-    listadoDeCompra.splice(0, listadoDeCompra.length);
-  
-    Swal.fire({
-      icon: tipo === "pagar" ? "success" : "info",
-      background: "#153081",
-      color: "#eaeaea",
-      title: `Proceso de compra <br> ${
-        tipo === "pagar" ? "COMPLETADO" : "CANCELADO"
-      }.`,
-      showConfirmButton: true,
-    });
-  
-    document
-      .querySelectorAll(".js-boton-cancelar-producto")
-      .forEach((boton) => {
-        const productoId = boton.dataset.productoId;
-        boton.innerHTML = "AGREGAR";
-        boton.classList.remove("js-boton-cancelar-producto");
-        boton.classList.add("js-boton-agregar-producto");
-        localStorage.removeItem(`boton-${productoId}`);
-      });
-  
-    localStorage.removeItem("productos");
-  
-    actualizarTotales([]);
-    localStorage.removeItem("listadoDeCompra");
-  }
-  
-  
-  
-  function agregarEventoBoton(tipo) {
-    if (tipo === null || tipo === undefined) return;
-    document
-      .querySelector(`.js-boton-${tipo}-compra`)
-      .addEventListener("click", () => {
-        Swal.fire({
-          title: `¿Desea ${tipo.toUpperCase()} su compra?`,
-          icon: "question",
-          background: "#153081",
-          color: "#eaeaea",
-          showCancelButton: true,
-          confirmButtonText: "Confirmar",
-          denyButtonText: "Cancelar",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            restablecerCompra(tipo);
-          }
-        });
-      });
-  }
+  restablecerCompra(tipo);
+  agregarEventoBoton(tipo);
   agregarEventoBoton("pagar");
   agregarEventoBoton("cancelar");
 
