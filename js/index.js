2af97bed7ac7772d6bd5c7b8a807fabb9f9be35b
tomasjrl <tstabilini@gmail.com>
2024-08-11 03:00:18 -0300

    cambio el orden de ejecucion de llamados a funciones

--- js/index.js
+++ js/index.js
@@ -5,19 +5,19 @@ import { actualizarTotales , listadoDeCompra } from "./utils/cuenta-compras.js";
 import { procesoCompra } from "./utils/proceso-compras.js";
 import {toggleMode} from "./html/dark-mode.js";
 
+/* llamo a la funcion que permite cambiar modo claro-oscuro */
+toggleMode();
+
 /* llamo a la funcion para generar el contenido HTML que estará dentro de <section class="hero-section"> 
 Aquí esta ubicado el listado de compras, las cuentas y la opción de pagar/cancelar la operación*/
 crearHeroSection();
 
-/* llamo a la funcion que permite generar el contenido HTML que comprende a las cartas de productos
-que estarán dentro de <section class="productos-grid js-productos-grid"> */
-ordenarProductos();
-
 /* llamo a la funcion que permite buscar/encontrar productos según parámetros de búsqueda*/
 buscarProductos();
 
-/* llamo a la funcion que permite cambiar modo claro-oscuro */
-toggleMode();
+/* llamo a la funcion que permite generar el contenido HTML que comprende a las cartas de productos
+que estarán dentro de <section class="productos-grid js-productos-grid"> */
+ordenarProductos();
 
 /* llamo a la funcion que permite actualizar las cuentas luego de agregar/quitar productos al listado de compras*/
 actualizarTotales();
