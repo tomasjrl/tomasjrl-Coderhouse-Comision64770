c4728c39fb161363baedfefa2bdfa285734948b8
tomasjrl <tstabilini@gmail.com>
2024-08-27 00:38:14 -0300

    separo en funciones exportadas el codigo referido al try-catch

--- js/utils/boton-pagar.js
+++ js/utils/boton-pagar.js
@@ -1,4 +1,11 @@
 import { listadoDeCompra, actualizarTotales } from "./cuenta-compras.js";
+import {
+  manejarErrorPago,
+  mostrarMensajeConfirmacion,
+  mensajeFinal,
+  mensajePago,
+  procesandoPago,
+} from "./mensajes-sweetalert.js";
 
 // función para restablecer la compra ya sea habiendo pagado o cancelado la compra
 
@@ -16,17 +23,7 @@ export function restablecerCompra(tipo) {
 
   listadoDeCompra.splice(0, listadoDeCompra.length);
 
-  // texto popup para informar que la operación final fue completada o cancelada
-
-  Swal.fire({
-    icon: tipo === "pagar" ? "success" : "info",
-    background: "#1e4180",
-    color: "#eaeaea",
-    title: `Proceso de compra <br> ${
-      tipo === "pagar" ? "COMPLETADO" : "CANCELADO"
-    }.`,
-    showConfirmButton: true,
-  });
+  mensajeFinal(tipo); // texto popup para informar que la operación final fue completada o cancelada
 
   // devuelvo clases originales a las etiquetas de los botones
 
@@ -54,76 +51,21 @@ export function agregarEventoBoton(tipo) {
     .addEventListener("click", () => {
       if (tipo.toUpperCase() === "PAGAR") {
         try {
-
-          // mensajes popup para concretar/cancelar el pago de compra
-
-          Swal.fire({
-            title: `¿Desea ${tipo.toUpperCase()} su compra?`,
-            html: `
-              <form style="display: flex; flex-direction: column;">
-                <input type="text" id="nombre" autocomplete="name" value="Tomás Stabilini" placeholder="Nombre" style="width: 100%; margin-bottom: 10px;">
-                <input type="email" id="email" autocomplete="email" value="Comision64770@coderhouse.com" placeholder="Correo electrónico" style="width: 100%; margin-bottom: 10px;">
-              </form>
-            `,
-            icon: "question",
-            background: "#1e4180",
-            color: "#eaeaea",
-            showCancelButton: true,
-            confirmButtonText: "Confirmar",
-            denyButtonText: "Cancelar",
-            preConfirm: () => {
-              const nombre = document.getElementById("nombre").value;
-              const email = document.getElementById("email").value;
-              if (!nombre || !email) {
-                Swal.showValidationMessage("Por favor, complete ambos campos");
-              } else if (!email.includes("@")) {
-                Swal.showValidationMessage(
-                  "Por favor, ingrese un correo electrónico válido"
-                );
-              }
-              return { nombre, email };
-            },
-          }).then((result) => {
+          // mensaje popup para concretar/cancelar el pago de compra
+          mensajePago(tipo).then((result) => {
             if (result.isConfirmed) {
-              Swal.fire({
-                title: "Procesando su pago...",
-                html: "Por favor, espere...",
-                background: "#1e4180",
-                color: "#eaeaea",
-                didOpen: () => {
-                  Swal.showLoading();
-                  Swal.getContainer()
-                    .querySelector(".swal-title")
-                    ?.classList.add("loading");
-                },
-                timer: 1250,
-              }).then(() => {
-                restablecerCompra(tipo);
+              procesandoPago().then(() => {
+                restablecerCompra(tipo); // función para restablecer valores
               });
             }
           });
         } catch (error) {
-          Swal.fire({
-            title: "Error al procesar el pago",
-            text: "Por favor, inténtelo nuevamente más tarde",
-            icon: "error",
-            background: "#1e4180",
-            color: "#eaeaea",
-          });
+          manejarErrorPago(); // mensaje popup por si hay error
         }
       } else {
-        Swal.fire({
-          
-          title: `¿Desea ${tipo.toUpperCase()} su compra?`,
-          icon: "question",
-          background: "#1e4180",
-          color: "#eaeaea",
-          showCancelButton: true,
-          confirmButtonText: "Confirmar",
-          denyButtonText: "Cancelar",
-        }).then((result) => {
+        mostrarMensajeConfirmacion(tipo).then((result) => {
           if (result.isConfirmed) {
-            restablecerCompra(tipo);
+            restablecerCompra(tipo); // función para restablecer valores
           }
         });
       }
---
+++ js/utils/mensajes-sweetalert.js
@@ -0,0 +1,89 @@
+// Función para manejar errores al procesar el pago
+
+export function manejarErrorPago() {
+  Swal.fire({
+    title: "Error al procesar el pago",
+    text: "Por favor, inténtelo nuevamente más tarde",
+    icon: "error",
+    background: "#1e4180",
+    color: "#eaeaea",
+  });
+}
+
+//Función para mostrar mensaje si concreta o si cancela el pago
+
+export function mensajePago(tipo) {
+  return Swal.fire({
+    title: `¿Desea ${tipo.toUpperCase()} su compra?`,
+    html: `
+      <form style="display: flex; flex-direction: column;">
+        <input type="text" id="nombre" autocomplete="name" value="Tomás Stabilini" placeholder="Nombre" style="width: 100%; margin-bottom: 10px;">
+        <input type="email" id="email" autocomplete="email" value="Comision64770@coderhouse.com" placeholder="Correo electrónico" style="width: 100%; margin-bottom: 10px;">
+      </form>
+    `,
+    icon: "question",
+    background: "#1e4180",
+    color: "#eaeaea",
+    showCancelButton: true,
+    confirmButtonText: "Confirmar",
+    denyButtonText: "Cancelar",
+    preConfirm: () => {
+      const nombre = document.getElementById("nombre").value;
+      const email = document.getElementById("email").value;
+      if (!nombre || !email) {
+        Swal.showValidationMessage("Por favor, complete ambos campos");
+      } else if (!email.includes("@")) {
+        Swal.showValidationMessage(
+          "Por favor, ingrese un correo electrónico válido"
+        );
+      }
+      return { nombre, email };
+    },
+  });
+}
+
+// Función para preguntar si confirma el pago
+
+export function mostrarMensajeConfirmacion(tipo) {
+  return Swal.fire({
+    title: `¿Desea ${tipo.toUpperCase()} su compra?`,
+    icon: "question",
+    background: "#1e4180",
+    color: "#eaeaea",
+    showCancelButton: true,
+    confirmButtonText: "Confirmar",
+    denyButtonText: "Cancelar",
+  });
+}
+
+// Función procesando pago
+
+export function procesandoPago() {
+  return Swal.fire({
+    title: "Procesando su pago...",
+    html: "Por favor, espere...",
+    background: "#1e4180",
+    color: "#eaeaea",
+    didOpen: () => {
+      Swal.showLoading();
+      Swal.getContainer()
+        .querySelector(".swal-title")
+        ?.classList.add("loading");
+    },
+    timer: 1250,
+  });
+}
+
+// Función mensaje final con pago concretado o cancelado
+
+export function mensajeFinal(tipo) {
+  Swal.fire({
+    icon: tipo === "pagar" ? "success" : "info",
+    background: "#1e4180",
+    color: "#eaeaea",
+    title: `Proceso de compra <br> ${
+      tipo === "pagar" ? "COMPLETADO" : "CANCELADO"
+    }.`,
+    showConfirmButton: true,
+  });
+}
